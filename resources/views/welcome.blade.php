1a5d23d9a1df2b56744cd424cce4e3a09f838455
Shawkat Ali <shawkat.ali@netmediamobile.com>
2026-05-30 18:18:13 +0600

    footer done

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -271,113 +271,55 @@
                                                         <span class="section-arrow-text">Trusted Brands</span>
                                                     </div>
                                                 </h3>
-                                                <div class="row product-grid main-products model-content">
+                                                <div class="row tb-product-grid">
                                                     @foreach($homeProducts as $product)
-                                                        <div class="col-xs-6 col-sm-6 col-md-3">
-                                                            <div class="product-layout has-extra-button">
-                                                                <div class="product-thumb">
-                                                                    <div class="image">
-                                                                        <div class="quickview-button">
-                                                                            <a class="btn btn-quickview" data-toggle="tooltip" data-tooltip-class="module-products-302 module-products-grid quickview-tooltip" data-placement="top" title="" onclick="quickview('2053')" data-original-title="Quickview">
-                                                                                <span class="btn-text">Quickview</span>
-                                                                            </a>
-                                                                        </div>
+                                                        <div class="col-xs-12 col-sm-6 col-md-6 tb-col">
+                                                            <div class="tb-card">
+                                                                {{-- Product Image --}}
+                                                                <a href="{{ url('product/'.$product->slug) }}" class="tb-img-wrap">
+                                                                    <img src="{{ asset('public/'.$product->first_image_url) }}"
+                                                                         alt="{{ $product->name }}"
+                                                                         class="tb-img" />
+                                                                </a>
 
-                                                                        <a href="{{ url('product/'.$product->slug) }}" class="product-img has-second-image">
-                                                                            <div>
-                                                                                <img src="{{ asset('public/'.$product->first_image_url) }}"
-                                                                                    srcset="{{ asset('public/'.$product->first_image_url) }}" width="250" height="250"
-                                                                                    alt="{{ $product->name }}" title="{{ $product->name }}"
-                                                                                    class="img-responsive img-first" />
+                                                                {{-- Product Name --}}
+                                                                <div class="tb-name">
+                                                                    <a href="{{ url('product/'.$product->slug) }}">{{ $product->name }}</a>
+                                                                </div>
 
-                                                                                @if(!empty($product->second_image_url))
-                                                                                    <img src="{{ asset('public/'.$product->second_image_url) }}"
-                                                                                        width="250" height="250"
-                                                                                        alt="{{ $product->name }}"
-                                                                                        title="{{ $product->name }}"
-                                                                                        class="img-responsive img-second" />
-                                                                                @else
-                                                                                    <img src="{{ asset('public/'.$product->first_image_url) }}"
-                                                                                        width="250" height="250"
-                                                                                        alt="{{ $product->name }}"
-                                                                                        title="{{ $product->name }}"
-                                                                                        class="img-responsive img-second" />
-                                                                                @endif
-                                                                            </div>
-                                                                        </a>
-                                                                    </div>
+                                                                {{-- Price Row --}}
+                                                                <div class="tb-price-row">
+                                                                    <span class="tb-price-label">Price</span>
+                                                                    @if(!$product->no_sale_price)
+                                                                        <span class="tb-price-badge">৳ {{ $product->sale_price }}</span>
+                                                                    @else
+                                                                        <button class="tb-call-btn" data-toggle="modal" data-target="#whatsappModal" data-product="{{ $product->name }}">
+                                                                            <svg width="13" height="13" viewBox="0 0 32 32" fill="#fff" style="vertical-align:middle;margin-right:4px;"><path d="M26.576 5.363c-2.69-2.69-6.406-4.354-10.511-4.354-8.209 0-14.865 6.655-14.865 14.865 0 2.732.737 5.291 2.022 7.491l-2.147 7.839 8.031-2.107a14.86 14.86 0 0 0 7.107 1.81h.006c8.209 0 14.862-6.656 14.862-14.865 0-4.103-1.662-7.817-4.505-10.679z"/></svg>
+                                                                            Please call us
+                                                                        </button>
+                                                                    @endif
+                                                                </div>
 
-                                                                    <div class="caption">
-                                                                        <div class="name">
-                                                                            <a class="truncate-text" href="#">
-                                                                                {{ $product->name }}
-                                                                            </a>
-                                                                        </div>
-
-                                                                        <div>
-                                                                            @if(!$product->no_sale_price)
-                                                                                <div class="price">
-                                                                                    <div>
-                                                                                        <span class="price-normal">{{ $product->sale_price }}৳</span>
-                                                                                    </div>
-                                                                                </div>
-
-                                                                                <div class="buttons-wrapper">
-                                                                                    <div class="button-group">
-                                                                                        <div class="cart-group">
-                                                                                            <div class="stepper">
-                                                                                                <input type="text" name="quantity" value="1" id="product-quantity-{{ $product->id }}" data-minimum="1" class="form-control" />
-                                                                                                <input type="hidden" name="product_id" value="{{ $product->id }}" />
-                                                                                                <span>
-                                                                                                    <i class="fa fa-angle-up"></i>
-                                                                                                    <i class="fa fa-angle-down"></i>
-                                                                                                </span>
-                                                                                            </div>
-
-                                                                                            <div>
-                                                                                                <a class="btn btn-cart" data-id="{{ $product->id }}" onclick="addToCart({{ $product->id }})">
-                                                                                                    <span class="btn-text">Add to Cart</span>
-                                                                                                </a>
-                                                                                            </div>
-                                                                                        </div>
-
-                                                                                        <div class="wish-group">
-                                                                                            <a class="btn btn-wishlist" data-id="{{ $product->id }}" onclick="addTowishlist({{ $product->id }})" data-original-title="Add to Wish List">
-                                                                                                <span class="btn-text">Add to Wish List</span>
-                                                                                            </a>
-
-                                                                                            <a class="btn btn-compare" data-id="{{ $product->id }}" data-toggle="tooltip" data-tooltip-class="product-grid compare-tooltip" data-placement="top" title="" onclick="addCompareList({{ $product->id }})">
-                                                                                                <span class="btn-text">Compare this Product</span>
-                                                                                            </a>
-                                                                                        </div>
-                                                                                    </div>
-                                                                                </div>
-                                                                            @else
-                                                                                <div class="price">
-                                                                                    <div>
-                                                                                        <span class="price-normal">Price on Request</span>
-                                                                                    </div>
-                                                                                </div>
-                                                                                <div class="text-center mt-2">
-                                                                                    <button class="btn btn-cart" data-toggle="modal" data-target="#whatsappModal" data-product="{{ $product->name }}">
-                                                                                        <svg fill="#FFFFFF" width="15px" height="15px" viewBox="0 0 32 32" version="1.1" xmlns="http://www.w3.org/2000/svg">
-                                                                                        <path d="M26.576 5.363c-2.69-2.69-6.406-4.354-10.511-4.354-8.209 0-14.865 6.655-14.865 14.865 0 2.732 0.737 5.291 2.022 7.491l-0.038-0.070-2.109 7.702 7.879-2.067c2.051 1.139 4.498 1.809 7.102 1.809h0.006c8.209-0.003 14.862-6.659 14.862-14.868 0-4.103-1.662-7.817-4.349-10.507l0 0zM16.062 28.228h-0.005c-0 0-0.001 0-0.001 0-2.319 0-4.489-0.64-6.342-1.753l0.056 0.031-0.451-0.267-4.675 1.227 1.247-4.559-0.294-0.467c-1.185-1.862-1.889-4.131-1.889-6.565 0-6.822 5.531-12.353 12.353-12.353s12.353 5.531 12.353 12.353c0 6.822-5.53 12.353-12.353 12.353h-0zM22.838 18.977c-0.371-0.186-2.197-1.083-2.537-1.208-0.341-0.124-0.589-0.185-0.837 0.187-0.246 0.371-0.958 1.207-1.175 1.455-0.216 0.249-0.434 0.279-0.805 0.094-1.15-0.466-2.138-1.087-2.997-1.852l0.010 0.009c-0.799-0.74-1.484-1.587-2.037-2.521l-0.028-0.052c-0.216-0.371-0.023-0.572 0.162-0.757 0.167-0.166 0.372-0.434 0.557-0.65 0.146-0.179 0.271-0.384 0.366-0.604l0.006-0.017c0.043-0.087 0.068-0.188 0.068-0.296 0-0.131-0.037-0.253-0.101-0.357l0.002 0.003c-0.094-0.186-0.836-2.014-1.145-2.758-0.302-0.724-0.609-0.625-0.836-0.637-0.216-0.010-0.464-0.012-0.712-0.012-0.395 0.010-0.746 0.188-0.988 0.463l-0.001 0.002c-0.802 0.761-1.3 1.834-1.3 3.023 0 0.026 0 0.053 0.001 0.079l-0-0.004c0.131 1.467 0.681 2.784 1.527 3.857l-0.012-0.015c1.604 2.379 3.742 4.282 6.251 5.564l0.094 0.043c0.548 0.248 1.25 0.513 1.968 0.74l0.149 0.041c0.442 0.14 0.951 0.221 1.479 0.221 0.303 0 0.601-0.027 0.889-0.078l-0.031 0.004c1.069-0.223 1.956-0.868 2.497-1.749l0.009-0.017c0.165-0.366 0.261-0.793 0.261-1.242 0-0.185-0.016-0.366-0.047-0.542l0.003 0.019c-0.092-0.155-0.34-0.247-0.712-0.434z"></path>
-                                                                                        </svg> &nbsp;
-                                                                                        Contact via WhatsApp
-                                                                                    </button>
-                                                                                </div>
-                                                                            @endif
-
-                                                                        </div>
-                                                                        
-                                                                    </div>
+                                                                {{-- Action Buttons --}}
+                                                                <div class="tb-actions">
+                                                                    @if(!$product->no_sale_price)
+                                                                        <a class="tb-btn-cart" onclick="addToCart({{ $product->id }})">Add to Cart</a>
+                                                                        <a class="tb-btn-buy" href="{{ url('product/'.$product->slug) }}">Buy Now</a>
+                                                                    @endif
+                                                                    <a class="tb-btn-icon tb-btn-wish" onclick="addTowishlist({{ $product->id }})" title="Wishlist">
+                                                                        <svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"><path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"></path></svg>
+                                                                    </a>
+                                                                    <a class="tb-btn-icon tb-btn-wa" data-toggle="modal" data-target="#whatsappModal" data-product="{{ $product->name }}" title="WhatsApp">
+                                                                        <svg width="16" height="16" viewBox="0 0 32 32" fill="currentColor"><path d="M26.576 5.363c-2.69-2.69-6.406-4.354-10.511-4.354-8.209 0-14.865 6.655-14.865 14.865 0 2.732.737 5.291 2.022 7.491l-2.147 7.839 8.031-2.107a14.86 14.86 0 0 0 7.107 1.81h.006c8.209 0 14.862-6.656 14.862-14.865 0-4.103-1.662-7.817-4.505-10.679z"/></svg>
+                                                                    </a>
                                                                 </div>
                                                             </div>
-                                                        </div> 
+                                                        </div>
                                                     @endforeach
                                                 </div>
                                             </div>
-                                             <!-- end top brands product -->
+                                            <!-- end trusted brands -->
+
                                         </div>
                                     </div>
                                 </div>
