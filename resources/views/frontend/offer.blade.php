11c316240a2cc9c16367e19224241320e16e353f
Shawkat Ali <shawkat.ali@netmediamobile.com>
2026-07-10 18:48:23 +0600

    done

--- resources/views/frontend/offer.blade.php
+++ resources/views/frontend/offer.blade.php
@@ -215,6 +215,7 @@
 
                                                         <div>
                                                             <a class="btn btn-cart" data-id="{{ $product->id }}" onclick="addToCart({{ $product->id }})">
+                                                                <svg class="btn-cart-icon" xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><circle cx="9" cy="21" r="1"/><circle cx="20" cy="21" r="1"/><path d="M1 1h4l2.68 13.39a2 2 0 0 0 2 1.61h9.72a2 2 0 0 0 2-1.61L23 6H6"/></svg> 
                                                                 <span class="btn-text">Add to Cart</span>
                                                             </a>
                                                         </div>
